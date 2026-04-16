b7c23d6a6d8e82c7248996ba4326ef82b7f1e254
wenjingding52 <wenjingding98@gmail.com>
2026-04-16 18:46:43 +0800

    Add files via upload

--- generate_wordcloud.py
+++ generate_wordcloud.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from wordcloud import WordCloud, STOPWORDS
+from collections import Counter
 import config
+import re
 
 def generate_news_wordclouds():
     # 1. Load data
@@ -26,18 +28,35 @@ def generate_news_wordclouds():
     if num_labels == 1:
         axes = [axes]
     
+    all_label_words = []
+    for label_val in labels:
+        raw_text = " ".join(df[df[config.LABEL_COLUMN] == label_val][config.TEXT_COLUMN].astype(str)).lower()
+        words = re.findall(r'\b[a-z]{3,}\b', raw_text) 
+        top_words = set([w for w, count in Counter(words).most_common(300)])
+        all_label_words.append(top_words)
+
+    common_high_freq = set.intersection(*all_label_words) if all_label_words else set()
+    
+    custom_stopwords = set(STOPWORDS).union(common_high_freq)
+    persistent_words = [
+        'friend', 'source', 'come', 'take', 'look', 'week', 'month', 
+        'year', 'said', 'will', 'even', 'around', 'another', 'thing',
+        're', 'u', 's', 'm', 't'
+    ]
     # 4. Generate a word cloud
     for i, label_value in enumerate(labels):
         print(f" {label_value}...")
         
-        text = " ".join(df[df[config.LABEL_COLUMN] == label_value][config.TEXT_COLUMN].astype(str))
+        raw_text = " ".join(df[df[config.LABEL_COLUMN] == label_value][config.TEXT_COLUMN].astype(str)).lower()
+        clean_text = " ".join(re.findall(r'\b[a-z]{3,}\b', raw_text))
         
         wc = WordCloud(
             width=800, height=800, 
             background_color='white', 
-            stopwords=STOPWORDS,
-            max_words=100
-        ).generate(text)
+            stopwords=custom_stopwords,
+            max_words=100,
+            regexp=r"\w{3,}"
+        ).generate(clean_text)
         
         axes[i].imshow(wc, interpolation='bilinear')
         axes[i].set_title(f"Label: {label_value}", fontsize=20)
